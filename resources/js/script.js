9d39651767d6a86191e23065675ec0df22595075
Junior <festerjunior@hotmail.com>
2017-01-27 21:04:38 -0800

    finished responsive additions, believe i fixed broken links

--- resources/js/script.js
+++ resources/js/script.js
@@ -61,4 +61,19 @@ $(document).ready(function() {
     }, {
         offset: '50%'
     });
+    
+    /*MOBILE NAV */
+    $('.js--nav-icon').click(function() { 
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon-i');
+        
+    nav.slideToggle(200);
+        if (icon.hasClass('ion-navicon-round')) {
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round');
+        } else {
+            icon.addClass('ion-navicon-round');
+            icon.removeClass('ion-close-round');
+        }
+    });
 });
