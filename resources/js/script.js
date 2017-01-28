f5c5d17988a7920ee42655f0f5861a528595805d
Junior <festerjunior@hotmail.com>
2017-01-28 13:11:43 -0800

    started adding map to page, just added js file and edited srcipt js so far

--- resources/js/script.js
+++ resources/js/script.js
@@ -75,5 +75,11 @@ $(document).ready(function() {
             icon.addClass('ion-navicon-round');
             icon.removeClass('ion-close-round');
         }
+        /* Maps */
+        new GMaps({
+          div: '#map',
+          lat: -12.043333,
+          lng: -77.028333
+        });
     });
 });
